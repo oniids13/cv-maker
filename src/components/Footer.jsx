682e193722e305425352fb1bd28fee2f961e37e0
oniids <jdino.abaya@gmail.com>
2025-05-13 19:23:19 +0800

    redesign the project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,32 @@
-
+import { GitHub, LinkedIn, Code } from "@mui/icons-material";
 
 function Footer() {
-    return (
-        <footer>
-            © {new Date().getFullYear()} | Jose Dino Abaya
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="footer-content">
+        <div className="copyright">
+          © {new Date().getFullYear()} | Jose Dino Abaya
+        </div>
+        <div className="social-links">
+          <a href="https://github.com/oniids13" aria-label="GitHub Profile">
+            <GitHub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/oniids/"
+            aria-label="LinkedIn Profile"
+          >
+            <LinkedIn />
+          </a>
+          <a
+            href="https://jose-dino-portfolio-react.vercel.app/"
+            aria-label="Portfolio"
+          >
+            <Code />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-
-export default Footer
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
+import { Description as DescriptionIcon } from "@mui/icons-material";
 
 function Header() {
-    return (
-        <>
-        <header>
-            <h1>CV Maker</h1>
-        </header>
-        </>
-    )
+  return (
+    <header>
+      <div className="header-content">
+        <DescriptionIcon fontSize="large" />
+        <h1>CV Builder</h1>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
