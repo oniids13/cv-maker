2ad23f89ae1b24f81ee5a13eb62dc537519ad75f
oniids <jdino.abaya@gmail.com>
2025-05-13 19:26:02 +0800

    fix links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,24 @@ function Footer() {
           © {new Date().getFullYear()} | Jose Dino Abaya
         </div>
         <div className="social-links">
-          <a href="https://github.com/oniids13" aria-label="GitHub Profile">
+          <a
+            href="https://github.com/oniids13"
+            aria-label="GitHub Profile"
+            target="_blank"
+          >
             <GitHub />
           </a>
           <a
             href="https://www.linkedin.com/in/oniids/"
             aria-label="LinkedIn Profile"
+            target="_blank"
           >
             <LinkedIn />
           </a>
           <a
             href="https://jose-dino-portfolio-react.vercel.app/"
             aria-label="Portfolio"
+            target="_blank"
           >
             <Code />
           </a>
